3d6d073d185897ae1ffb11f5d5630aef53203b02
Dipto <diptanath44@gmail.com>
2020-01-18 22:50:34 +0600

    caffe

--- app/src/main/java/dev/encephalon/espy/data/caffe/CaffeList.kt
+++ app/src/main/java/dev/encephalon/espy/data/caffe/CaffeList.kt
@@ -1,4 +1,12 @@
 package dev.encephalon.espy.data.caffe
 
 object CaffeList {
+    fun getList(): List<Caffes>{
+        val imageList = ArrayList<Caffes>()
+        imageList.clear()
+        imageList.add(
+            Caffes()
+        )
+        return imageList
+    }
 }
--- app/src/main/java/dev/encephalon/espy/data/caffe/Caffes.kt
+++ app/src/main/java/dev/encephalon/espy/data/caffe/Caffes.kt
@@ -1,2 +1,6 @@
 package dev.encephalon.espy.data.caffe
 
+data class Caffes(
+    val url: String,
+    val name: String
+)
